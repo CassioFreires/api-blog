05d7d28d932fea3249d98d645e29da740242a6b6
Cassio Souza <cassio.souza@ancar.local>
2025-08-20 15:53:26 -0300

    Removendo dist do repositório

--- dist/modules/permission/schema/create-permission.schema.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createPermissionSchema = void 0;
-const zod_1 = require("zod");
-exports.createPermissionSchema = zod_1.z.object({
-    name: zod_1.z.string().min(1, { message: 'O nome é obrigatório' }),
-    description: zod_1.z.string().min(1, { message: 'A descrição é obrigatório' }),
-});
